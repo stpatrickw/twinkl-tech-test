8b80a3903ef2502c36f4251e4f7b4f6f1774cad5
Andrey Demidenko <stpatrickw@gmail.com>
2024-09-26 11:33:46 +0300

    Add Symfony framework with DB and Mailhog config in .env

---
+++ symfony_app/src/Kernel.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\Kernel as BaseKernel;
+
+class Kernel extends BaseKernel implements CompilerPassInterface
+{
+    use MicroKernelTrait;
+
+    public function process(ContainerBuilder $container)
+    {
+        // TODO: Implement process() method.
+    }
+}
